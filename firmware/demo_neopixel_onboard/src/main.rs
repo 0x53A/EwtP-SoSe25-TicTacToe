a3b8695a67437e2b6a9f106bcebe6e4d02f8d85e
Lukas Rieger <code@lukasrieger.com>
2025-09-13 22:39:05 +0200

    reduce warnings

--- firmware/demo_neopixel_onboard/src/main.rs
+++ firmware/demo_neopixel_onboard/src/main.rs
@@ -3,33 +3,19 @@
 #![feature(never_type)]
 
 use esp_backtrace as _;
-use esp_hal::{
-    delay::Delay,
-    dma_buffers,
-    gpio::Io,
-    i2s::master::{DataFormat, Standard},
-    main,
-    peripherals::Peripherals,
-    time::Rate,
-    uart::Uart,
-};
+use esp_hal::{delay::Delay, main, peripherals::Peripherals, time::Rate};
 
 use anyhow::{Result, anyhow};
 use esp_println::println;
-use heapless::Vec;
 
 use esp_alloc as _;
-use microfft::real::rfft_512;
-use smart_leds::RGB8;
 use smart_leds::SmartLedsWrite;
 
 use embedded_hal::delay::DelayNs;
 
 type NeopixelT<'a> = ws2812_spi::Ws2812<esp_hal::spi::master::Spi<'a, esp_hal::Blocking>>;
 
-
 esp_bootloader_esp_idf::esp_app_desc!();
-
 
 #[main]
 fn main() -> ! {
@@ -40,13 +26,11 @@ fn main() -> ! {
     }
 }
 
-
 fn _main() -> Result<!> {
     esp_alloc::heap_allocator!(size: 72 * 1024);
 
     let peripherals: Peripherals = esp_hal::init(esp_hal::Config::default());
     let mut delay = Delay::new();
-    let io = Io::new(peripherals.IO_MUX);
 
     // Setup SPI for NeoPixel
     let spi = esp_hal::spi::master::Spi::new(
@@ -57,35 +41,19 @@ fn _main() -> Result<!> {
 
     let mut neopixel: NeopixelT = ws2812_spi::Ws2812::new(spi);
 
-    // Setup UART
-    let config = esp_hal::uart::Config::default().with_baudrate(115200);
-    let mut uart = Uart::new(peripherals.UART1, config)?
-        .with_rx(peripherals.GPIO17)
-        .with_tx(peripherals.GPIO8);
-
     let blue = smart_leds::colors::BLUE;
     let green = smart_leds::colors::GREEN;
     let red = smart_leds::colors::RED;
     neopixel.write([blue]).map_err(|err| anyhow!("{:?}", err))?;
 
     loop {
-        neopixel.write([green]).map_err(|err| anyhow!("{:?}", err))?;
+        neopixel
+            .write([green])
+            .map_err(|err| anyhow!("{:?}", err))?;
         delay.delay_ms(100);
         neopixel.write([blue]).map_err(|err| anyhow!("{:?}", err))?;
         delay.delay_ms(100);
         neopixel.write([red]).map_err(|err| anyhow!("{:?}", err))?;
         delay.delay_ms(100);
     }
-}
-
-
-fn update_led(r: u8, g: u8, b: u8, neopixel: &mut NeopixelT) -> Result<()> {
-    let color = RGB8::new(r, g, b);
-
-    // Update neopixel
-    neopixel
-        .write([color])
-        .map_err(|err| anyhow!("{:?}", err))?;
-
-    Ok(())
 }
